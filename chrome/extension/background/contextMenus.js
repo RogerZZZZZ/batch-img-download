9d907a8385460ca477f7a4f0961d120e9845d7dc
RogerZZZZZ <501841352@qq.com>
2018-10-18 11:53:31 +0800

    fix new tab can not find the correct path

--- chrome/extension/background/contextMenus.js
+++ chrome/extension/background/contextMenus.js
@@ -1,29 +1,29 @@
-let windowId = 0;
+// let windowId = 0;
 const CONTEXT_MENU_ID = 'example_context_menu';
 
-function closeIfExist() {
-  if (windowId > 0) {
-    chrome.windows.remove(windowId);
-    windowId = chrome.windows.WINDOW_ID_NONE;
-  }
-}
+// function closeIfExist() {
+//   if (windowId > 0) {
+//     chrome.windows.remove(windowId);
+//     windowId = chrome.windows.WINDOW_ID_NONE;
+//   }
+// }
 
-function popWindow(type) {
-  closeIfExist();
-  const options = {
-    type: 'popup',
-    left: 100,
-    top: 100,
-    width: 800,
-    height: 475,
-  };
-  if (type === 'open') {
-    options.url = 'window.html';
-    chrome.windows.create(options, (win) => {
-      windowId = win.id;
-    });
-  }
-}
+// function popWindow(type) {
+//   closeIfExist();
+//   const options = {
+//     type: 'popup',
+//     left: 100,
+//     top: 100,
+//     width: 800,
+//     height: 475,
+//   };
+//   if (type === 'open') {
+//     options.url = 'window.html';
+//     chrome.windows.create(options, (win) => {
+//       windowId = win.id;
+//     });
+//   }
+// }
 
 chrome.contextMenus.create({
   id: CONTEXT_MENU_ID,
@@ -34,8 +34,11 @@ chrome.contextMenus.create({
   ]
 });
 
-chrome.contextMenus.onClicked.addListener((event) => {
-  if (event.menuItemId === CONTEXT_MENU_ID) {
-    popWindow('open');
-  }
-});
+chrome.contextMenus.onClicked.addListener(() => {
+  // if (event.menuItemId === CONTEXT_MENU_ID) {
+  //   popWindow('open');
+  // }
+  chrome.tabs.create({
+    url: 'chrome://newtab'
+  })
+})
